47512593da7cb37f88e7a71d5166d0f183e98488
Ricardo Villagrana <ricardo.villagrana@tangosource.com>
2020-05-16 18:24:36 -0500

    test(model): add game model test

---
+++ test/models/game_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class GameTest < ActiveSupport::TestCase
+  def setup
+    @game = User.first.games.create(bombs: 15, result: :playing)
+  end
+
+  test 'the game has no cells before start' do
+    assert User.first.games.create.cells.count.zero?
+  end
+
+  test 'the games is populated after start' do
+    assert_equal(@game.cells.count, @game.cols * @game.rows)
+  end
+
+  test 'the games is populated with bombs after first reveal' do
+    @game.start_on!(3, 3)
+
+    assert_equal(@game.bomb_cells.count, @game.bombs)
+  end
+
+  test 'the games won\'t populate again if start_on! is used again' do
+    @game.start_on!(3, 4)
+
+    assert_equal(@game.bomb_cells.count, @game.bombs)
+  end
+
+  test 'the method reveal! delegates to cell' do
+    @game.start_on!(3, 3)
+    @game.reveal!(5, 5)
+
+    assert_not @game.cell(5, 5).hidden?
+  end
+
+  test 'the method flag! delegates to cell' do
+    @game.flag!(5, 5)
+
+    assert @game.cell(5, 5).flag?
+  end
+
+  test 'the method state brings the correct cell' do
+    assert_equal(@game.state(5, 5), @game.cell(5, 5).state)
+  end
+end
