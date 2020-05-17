58873e8248feb5ef9b0a0f8e92c99286ab0c550a
Ricardo Villagrana <ricardo.villagrana@tangosource.com>
2020-05-16 20:35:30 -0500

    fix: update current_user and verify_user! methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,14 @@ class ApplicationController < ActionController::API
   private
 
   def verify_user!
-    haed :unauthorized unless current_user
+    unless current_user
+      return render json: { error: 'You need to choose an username' }
+    end
   end
 
   def current_user
     user_id = request.headers['User-ID']
 
-    User.find(user_id)
+    User.where(id: user_id).first
   end
 end
